b6373bb0d994f085009ff0b3f3433dab0c403db5
IsaacGuevara <antonio.isaac.unam@gmail.com>
2026-05-07 19:22:31 -0600

    numero 2

--- clase_1/tipos_datos.js
+++ clase_1/tipos_datos.js
@@ -1,21 +1,21 @@
 //Tareas de la practica numero 1 
 
 
-console.log(typeof 42); // number
+console.log(typeof 42); // numero
 
 console.log(typeof 'Veinticinco'); // string
 
-console.log(typeof -666); // number
+console.log(typeof -666); // numbero
 
-console.log(typeof true); // boolean
+console.log(typeof true); // booleano
 
-console.log(typeof 0); // number
+console.log(typeof 0); // numbero
 
 console.log(typeof ''); // string
 
-console.log(typeof null); // object
+console.log(typeof null); // objeto
 
 console.log(typeof undefined); // undefined
 
-console.log(typeof false); // boolean
+console.log(typeof false); // booleano
 
